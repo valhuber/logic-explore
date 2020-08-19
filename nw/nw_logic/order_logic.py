6a3836fa828c9798be6a72306fd775764a3d8199
valhuber <valjhuber@gmail.com>
2020-08-19 10:59:50 -0700

    aggregates into db

--- nw/nw_logic/order_logic.py
+++ nw/nw_logic/order_logic.py
@@ -4,33 +4,6 @@ import sys
 import sqlalchemy
 
 # import logic_engine.logic
-sys_exec = sys.executable
-sys_path = str(sys.path)
-cwd = os.getcwd()
-
-# https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html#absolute-vs-relative-import
-print("Import INSANITY, cwd:        " + cwd)  # chrisyeh@cs.stanford.edu
-# print("Import INSANITY, sys_exec: " + sys_exec)  # its the venv python bin
-print("Import INSANITY, sys_path: " + sys_path)
-
-'''Works in pycharm
-Import INSANITY, cwd:        /Users/val/python/vsc/logic-explore
-Import INSANITY, sys_path: ['/Users/val/python/vsc/logic-explore/nw/nw_logic', ...
-'''
-
-'''fails in vsc using config.cwd=${workspaceFolder}, BUT same as pycharm!!
-Import INSANITY, cwd:        /Users/val/python/vsc/logic-explore
-Import INSANITY, sys_path: ['/Users/val/python/vsc/logic-explore/nw/nw_logic', ...
-'''
-
-''' fails as script:
-Import INSANITY, cwd:        /Users/val/python/vsc/logic-explore/nw/nw_logic
-Import INSANITY, sys_path: ['/Users/val/python/vsc/logic-explore/nw/nw_logic', 
-                '/Users/val/.pyenv/versions/3.8.3/lib/python38.zip', 
-                '/Users/val/.pyenv/versions/3.8.3/lib/python3.8', 
-                '/Users/val/.pyenv/versions/3.8.3/lib/python3.8/lib-dynload', 
-                '/Users/val/python/vsc/logic-explore/venv/lib/python3.8/site-packages']
-'''
 
 from logic_engine import logic  # see .env file (or pycharm Add Content Roots)
 from typing import NewType
@@ -68,7 +41,7 @@ def print_msg(msg):  # This is the outer enclosing function
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 basedir = os.path.dirname(basedir)
-conn_string = "sqlite:///" + os.path.join(basedir, "Northwind_small.sqlite")
+conn_string = "sqlite:///" + os.path.join(basedir, "nw.db")
 engine = sqlalchemy.create_engine(conn_string)
 
 # Create a session
