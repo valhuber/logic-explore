764af5a7d642babe4d61566e019b089eaef9af65
valhuber <valjhuber@gmail.com>
2020-08-22 16:56:29 -0700

    setAttr not required

--- nw/trans_tests/add_order.py
+++ nw/trans_tests/add_order.py
@@ -25,8 +25,7 @@ def add_order(a_session: session):
 
 def upd_order(a_session: session):
     test_order = a_session.query(models.Order).filter(models.Order.Id == 11011).one()
-    # test_order.ShippedDate = "2014-03-25"  # nope, nice try
-    setattr(test_order, 'ShippedDate', "2014-03-25")
+    test_order.ShippedDate = "2014-03-27"  #
     # ship this unshipped order (dates are like 2014-03-24)
     # a_session.query(models.Order).update(test_order)  # Order not iterable
     insp = inspect(test_order)
