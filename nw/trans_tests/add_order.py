d12911222ab47769958c760ad174b818f34a8244
valhuber <valjhuber@gmail.com>
2020-08-23 17:54:49 -0700

    validating new order adjusts cust
    fails - adjusted order not flushed

--- nw/trans_tests/add_order.py
+++ nw/trans_tests/add_order.py
@@ -8,6 +8,8 @@ from nw.nw_logic import session  # opens db, activates logic listener <--
 
 
 # Add Order - works
+pre_cust = session.query(models.Customer).filter(models.Customer.Id == "ALFKI").one()
+
 new_order = models.Order(AmountTotal=0, CustomerId="ALFKI", ShipCity="Richmond",
                          EmployeeId=6, Freight=1)
 session.add(new_order)
@@ -22,10 +24,23 @@ new_item2 = models.OrderDetail(ProductId=2, Amount=0,
                                Discount=0)
 new_order.OrderDetailList.append(new_item2)
 session.commit()
-print("\nadd_order, completed\n\n")
+
+post_cust = session.query(models.Customer).filter(models.Customer.Id == "ALFKI").one()
+
+print("\nadd_order, update completed\n\n")
 row_prt(new_order, "\nnew Order Result")  # $18 + $38 = $56
 if new_order.AmountTotal != 56:
     print ("==> ERROR - unexpected AmountTotal: " + str(new_order.AmountTotal) +
            "... expected 56")
 row_prt(new_item1, "\nnew Order Detail 1 Result")  # 1 Chai  @ $18
 row_prt(new_item2, "\nnew Order Detail 2 Result")  # 2 Chang @ $19 = $38
+
+if post_cust.Balance == pre_cust.Balance + 56:
+    row_prt(post_cust, "\nCorrect adjusted Customer Result")
+else:
+    row_prt(post_cust, "\nERROR - incorrect adjusted Customer Result")
+    print("\n--> probable cause: item's Order adjust before_flush logic does not run Order's flush")
+    row_prt(pre_cust, "\npre_cust")
+
+print("\nadd_order, ran to completion\n\n")
+
