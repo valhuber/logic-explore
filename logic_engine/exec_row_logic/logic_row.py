497ec7fc0662b40b9eae74115ec539bec1025738
valhuber <valjhuber@gmail.com>
2020-09-01 15:51:16 -0700

    LogicRow.__str__()

--- logic_engine/exec_row_logic/logic_row.py
+++ logic_engine/exec_row_logic/logic_row.py
@@ -3,8 +3,33 @@ class LogicRow:
     state for row logic execution (wraps row, with mold_row, ins_upd_dlt, nest_level, etc)
     """
 
-    def __init__(self, a_row, an_old_row, an_ins_upd_dlt, a_nest_level):
-        self.row = a_row
-        self.old_row = an_old_row
-        self.ins_upd_dlt = an_ins_upd_dlt
-        self.nest_level = a_nest_level
+    def __init__(self, row, old_row, ins_upd_dlt, nest_level):
+        self.row = row
+        self.old_row = old_row
+        self.ins_upd_dlt = ins_upd_dlt
+        self.nest_level = nest_level
+
+    def __str__(self):
+        result = self.row.__tablename__ + "[]: "  # TODO add [pk]
+        cols = self.row.__table__.columns
+        is_first = True
+        for each_col in cols:
+            each_col_name = each_col.name
+            if not is_first:
+                result += ", "
+            is_first = False
+            if each_col_name == "Idxx":
+                print("Debug Stop here")
+            value = getattr(self.row, each_col_name)
+            result += each_col_name + ": "
+            old_value = value
+            if self.old_row is not None:
+                old_value = getattr(self.old_row, each_col_name)
+            if value != str(old_value):
+                result += ' [' + str(old_value) + '-->]'
+            if isinstance(value, str):
+                result += value
+            else:
+                result += str(value)
+        return result  # str(my_dict)
+
--- nw/trans_tests/upd_order_shipped.py
+++ nw/trans_tests/upd_order_shipped.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy import inspect
 
 import nw.nw_logic.models as models
+from logic_engine.exec_row_logic.logic_row import LogicRow
 from nw.nw_logic import session  # opens db, activates logic listener <--
 
 
@@ -10,6 +11,9 @@ from nw.nw_logic import session  # opens db, activates logic listener <--
 """ also test join.
 session.query(Customer).join(Invoice).filter(Invoice.amount == 8500).all()
 """
+pre_cust = session.query(models.Customer).filter(models.Customer.Id == "ALFKI").one()
+session.expunge(pre_cust)
+
 test_order = session.query(models.Order).filter(models.Order.Id == 11011).join(models.Employee).one()
 if test_order.ShippedDate is None or test_order.ShippedDate == "":
     test_order.ShippedDate = str(datetime.now())
@@ -21,6 +25,11 @@ else:
 # a_session.query(models.Order).update(test_order)  # Order not iterable
 insp = inspect(test_order)
 session.commit()
+
+post_cust = session.query(models.Customer).filter(models.Customer.Id == "ALFKI").one()
+logic_row = LogicRow(row=pre_cust, old_row=post_cust, nest_level=0, ins_upd_dlt="*")
+debug = logic_row.__str__()
+print("Customer LogicRow: " + debug)
 print("\nupd_order, completed\n\n")
 # TODO check Customer Balance correctly adjusted
 
