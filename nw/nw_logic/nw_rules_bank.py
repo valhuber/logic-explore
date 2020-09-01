0d56a84c0b0d6fd796288109ca6ea6e56a847fe7
valhuber <valjhuber@gmail.com>
2020-08-31 21:02:59 -0700

    verify count deposit / withdrawal

--- nw/nw_logic/nw_rules_bank.py
+++ nw/nw_logic/nw_rules_bank.py
@@ -17,7 +17,8 @@ class NwLogic(object):
         return row.UnitPrice <= row.Quantity
 
     Logic.constraint_rule(validate="Customer", calling='check_balance')
-    Logic.sum_rule(derive="Customer.balance", as_sum_of="Order.AmountTotal", where="ShippedData not None")
+    Logic.sum_rule(derive="Customer.balance", as_sum_of="Order.AmountTotal", where="ShippedDate not None")
+    Logic.count_rule(derive="Customer.OrderCount", as_count_of="Order", where="ShippedDate not None")
     Logic.sum_rule(derive="Order.AmountTotal", as_sum_of="OrderDetails.Amount")
     Logic.formula_rule(derive="OrderDetail.Amount", calling="compute_amount")
     Logic.copy_rule(derive="OrderDetail.UnitPrice", from_parent="Product.UnitPrice")
